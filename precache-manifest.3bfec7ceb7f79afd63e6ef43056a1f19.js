c04231eee95236a9ae537150ece112e06f70432f
Alina Shilovets <36643977+shilovets@users.noreply.github.com>
2020-06-19 17:14:16 +0300

    Updates

---
+++ precache-manifest.3bfec7ceb7f79afd63e6ef43056a1f19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "361676f1ebf2c92f7a4f023c4842ea3c",
+    "url": "/image-storage/index.html"
+  },
+  {
+    "revision": "d1bd1c2dd3c9e98b242f",
+    "url": "/image-storage/static/css/main.e038fc92.chunk.css"
+  },
+  {
+    "revision": "398404c195bb061f0033",
+    "url": "/image-storage/static/js/2.97f55ba4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/image-storage/static/js/2.97f55ba4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1bd1c2dd3c9e98b242f",
+    "url": "/image-storage/static/js/main.016974a8.chunk.js"
+  },
+  {
+    "revision": "4bc9dbf50799e2389d74",
+    "url": "/image-storage/static/js/runtime-main.fad348d9.js"
+  }
+]);
